abf2464f40d69b4a8ff0d04dcdb56ffbc082456e
Torsten Friebe <torsten.friebe@gmail.com>
2018-10-24 18:57:43 +0200

    #4836 Enhanced CLI -h output describing usage

--- src/main/java/de/deegreeenterprise/tools/featurestoresql/FeaturestoreSqlApp.java
+++ src/main/java/de/deegreeenterprise/tools/featurestoresql/FeaturestoreSqlApp.java
@@ -16,9 +16,9 @@ public class FeaturestoreSqlApp {
     public static void main( String[] args ) {
         if ( args.length == 0 || "--help".equals( args[0] ) || "-help".equals( args[0] )  || "-h".equals( args[0] ) ) {
             printUsage();
-        } else if ( "featureStoreConfigLoader".equals( args[0] ) ) {
+        } else if ( "FeatureStoreConfigLoader".equalsIgnoreCase( args[0] ) ) {
             FeatureStoreLoaderConfigApp.run( args );
-        } else if ( "gmlLoader".equals( args[0] ) ) {
+        } else if ( "GmlLoader".equalsIgnoreCase( args[0] ) ) {
             GmlLoaderApp.run( args );
         } else {
             printUsage();
@@ -26,10 +26,10 @@ public class FeaturestoreSqlApp {
     }
 
     private static void printUsage() {
-        System.out.println( "Includes tools to create SQLFeatureStore configurations and load  GML files" );
-        System.out.println( "Use the keywords 'featureStoreConfigLoader' or 'gmlLoader' to differentiate between the two tools: " );
-        System.out.println( "   featureStoreConfigLoader -h (Prints the usage of this tool)" );
-        System.out.println( "   gmlLoader -h (Prints the usage of this tool)" );
+        System.out.println( "Includes tools to create SQLFeatureStore configurations and load GML files" );
+        System.out.println( "Use the keywords 'FeatureStoreConfigLoader' or 'GmlLoader' to choose between the tools: " );
+        System.out.println( "   FeatureStoreConfigLoader -h (Prints the usage for this tool)" );
+        System.out.println( "   GmlLoader -h (Prints the usage for this tool)" );
     }
 
 }
--- src/main/java/de/deegreeenterprise/tools/featurestoresql/loader/GmlLoaderHelpUsage.java
+++ src/main/java/de/deegreeenterprise/tools/featurestoresql/loader/GmlLoaderHelpUsage.java
@@ -6,14 +6,16 @@ package de.deegreeenterprise.tools.featurestoresql.loader;
 public class GmlLoaderHelpUsage {
 
     public static void printUsage() {
-        System.out.println( "Import of GML to deegree WFS" );
-        System.out.println( "Options (mandatory):" );
-        System.out.println( "   -pathToFile=/path/tp/file - the path to the GML file to import" );
-        System.out.println( "   -workspaceName=inspire - the name of the deegree workspace used for the import. Must be located at DEEGREE_ROOT" );
-        System.out.println( "   -sqlFeatureStoreId=cadastralparcels - the id of the SQLFeatureStore in the workspace" );
+        System.out.println( "Usage: java -jar deegree-tools-featurestore-sql.jar GmlLoader [options]" );
+        System.out.println( "Description: Imports a GML file directly into a given deegree SQLFeatureStore" );
+        System.out.println();
+        System.out.println( "options (all are mandatory):" );
+        System.out.println( " -pathToFile=<path/to/gmlfile> - the path to the GML file to import" );
+        System.out.println( " -workspaceName=<workspace_identifier> - the name of the deegree workspace used for the import. Must be located at default DEEGREE_WORKSPACE_ROOT directory" );
+        System.out.println( " -sqlFeatureStoreId=<feature_store_identifier> - the ID of the SQLFeatureStore in the given workspace" );
         System.out.println();
         System.out.println( "Example:" );
-        System.out.println( "-pathToFile=/path/to/cadastralparcels.gml -workspaceName=inspire -sqlFeatureStoreId=cadastralparcels" );
+        System.out.println( " java -jar deegree-tools-featurestore-sql.jar GmlLoader -pathToFile=/path/to/cadastralparcels.gml -workspaceName=inspire -sqlFeatureStoreId=cadastralparcels" );
     }
 
 }
