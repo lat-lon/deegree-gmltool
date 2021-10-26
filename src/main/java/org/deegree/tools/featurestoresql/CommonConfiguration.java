92cc087ad084f43571a6e90cd4efad1014dd6f4e
Lyn Elisa Goltz <goltz@lat-lon.de>
2021-10-26 12:31:18 +0200

    #7694 - renamed groupId; renamed package

---
+++ src/main/java/org/deegree/tools/featurestoresql/CommonConfiguration.java
@@ -0,0 +1,27 @@
+package org.deegree.tools.featurestoresql;
+
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.JobLauncherCommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz </a>
+ */
+@Configuration
+public class CommonConfiguration {
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private JobExplorer jobExplorer;
+
+    @Bean
+    public JobLauncherCommandLineRunner runner() {
+        return new JobLauncherCommandLineRunner( jobLauncher, jobExplorer );
+    }
+
+}
