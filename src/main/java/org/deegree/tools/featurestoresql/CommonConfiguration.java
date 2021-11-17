7aff7fefe2c746806195aaf93e87a136c68c6a11
Torsten Friebe <torsten.friebe@gmail.com>
2021-11-17 12:38:32 +0100

    Upgraded plugins

--- src/main/java/org/deegree/tools/featurestoresql/CommonConfiguration.java
+++ src/main/java/org/deegree/tools/featurestoresql/CommonConfiguration.java
@@ -2,6 +2,7 @@ package org.deegree.tools.featurestoresql;
 
 import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.batch.JobLauncherCommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -19,9 +20,12 @@ public class CommonConfiguration {
     @Autowired
     private JobExplorer jobExplorer;
 
+    @Autowired
+    private JobRepository jobRepository;
+
     @Bean
     public JobLauncherCommandLineRunner runner() {
-        return new JobLauncherCommandLineRunner( jobLauncher, jobExplorer );
+        return new JobLauncherCommandLineRunner( jobLauncher, jobExplorer, jobRepository );
     }
 
 }
